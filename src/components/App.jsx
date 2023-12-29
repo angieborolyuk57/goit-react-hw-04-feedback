9ba612721b4a6e9314beb6133d1f70cbe6584852
Anhelina Rabtsevych <anhelina@ondaytrip.com>
2023-12-29 14:48:16 +0100

    code refactoring with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,86 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component, useState} from 'react';
 import Statistics from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
 import Section from './Section';
 import css from './App.module.css';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
+const App = () => {
 
-        this.state = {
+const [feedback, setFeedback] = useState({
             good: 0,
             neutral: 0,
             bad: 0,
-        };
-    }
+        
+    })
 
-    handleClick = (type) => {
-        this.setState((prevState) => ({
-            [type]: prevState[type] + 1,
+    const handleClick = (type) => {
+        setFeedback((prevFeedback) => ({
+            ...prevFeedback,
+            [type]: prevFeedback[type] + 1,
         }));
     };
 
-    countTotalFeedback = () => {
-        const { good, neutral, bad } = this.state;
+    const countTotalFeedback = () => {
+        const { good, neutral, bad } = feedback;
         return good + neutral + bad;
     };
 
-    countPositiveFeedbackPercentage = () => {
-        const { good } = this.state;
-        const totalFeedback = this.countTotalFeedback();
+    const countPercentage = (value) => {
+        const totalFeedback = countTotalFeedback();
 
         if (totalFeedback === 0) {
             return 0;
         }
 
-        return Math.floor((good / totalFeedback) * 100);
+        return Math.floor((value / totalFeedback) * 100);
     };
-    countNeutralFeedbackPercentage = () => {
-      const { neutral } = this.state;
-      const totalFeedback = this.countTotalFeedback();
+
+    const countPositiveFeedbackPercentage = () => countPercentage(feedback.good);
+    const countNeutralFeedbackPercentage = () => countPercentage(feedback.neutral);
+    const countNegativeFeedbackPercentage = () => countPercentage(feedback.bad);
+
+    const totalFeedback = countTotalFeedback();
   
-      if (totalFeedback === 0) {
-          return 0;
-      }
-      return Math.floor((neutral / totalFeedback) * 100);
-  };
-  
-  countNegativeFeedbackPercentage = () => {
-    const { bad } = this.state;
-    const totalFeedback = this.countTotalFeedback();
-
-    if (totalFeedback === 0) {
-        return 0;
-    }
-
-    return Math.floor((bad / totalFeedback) * 100);
-};
-
-
-    render() {
-        const { good, neutral, bad } = this.state;
-        const totalFeedback = this.countTotalFeedback();
-
         return (
             <div className={css.feedback}>
                 <Section title="Please leave your feedback">
                     <FeedbackOptions
                         options={['good', 'neutral', 'bad']}
-                        onLeaveFeedback={this.handleClick}
+                        onLeaveFeedback={handleClick}
                     />
                 </Section>
 
                 <Section title="Statistics">
                     {totalFeedback > 0 ? (
                         <Statistics
-                            good={good}
-                            neutral={neutral}
-                            bad={bad}
+                            good={feedback.good}
+                            neutral={feedback.neutral}
+                            bad={feedback.bad}
                             total={totalFeedback}
-                            positivePercentage={this.countPositiveFeedbackPercentage()}
-                            neutralPercentage={this.countNeutralFeedbackPercentage()}
-                            negativePercentage={this.countNegativeFeedbackPercentage()}
+                            positivePercentage={countPositiveFeedbackPercentage()}
+                            neutralPercentage={countNeutralFeedbackPercentage()}
+                            negativePercentage={countNegativeFeedbackPercentage()}
                         />
                     ) : (
                         <p>No feedback given yet.</p>
@@ -89,6 +68,5 @@ class App extends Component {
             </div>
         );
     }
-}
 
 export default App;
